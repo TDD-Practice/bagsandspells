9afff7740e04b46bff808b470ba77ba51096936b
marcosantonastasi <marco.santonastasi@gmail.com>
2022-03-13 15:06:45 +0100

    fixed test of backpack overflow not being red

--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@ import pytest
 
 from main import Backpack, Bag1, Bag2, Bag3, Bag4
 
-def test_item_is_added_to_backpack():
+def item_is_added_to_backpack():
     Backpack.add_item("copper")
     assert len(Backpack.items) == 1
     assert "copper" in Backpack.items
@@ -10,7 +10,7 @@ def test_item_is_added_to_backpack():
 def test_item_is_added_to_Bag1_if_Backpack_is_full():
     for i in range(8):
         Backpack.add_item("copper")
-    assert len(Backpack.items) == 8
     Bag1.add_item("wood")
-    assert "wood" in Bag1.items
+    assert "wood" not in Bag1.items
+    assert len(Backpack.items) == 8
 
